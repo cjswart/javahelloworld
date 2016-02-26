8a599d5aea6958dc885d839bd51fbf795425ac9e
Ubuntu <cees-jan.swart@kpn.com>
2016-02-26 10:47:16 +0000

    New project

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main (String [] args) {
-    System.out.println("Hello Cees-Jan en Jeroen");
+    System.out.println("Hello Cees-Jan en Jeroen by docker automatic build");
     System.exit(0);
   }
 }
