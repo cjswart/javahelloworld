763621382a7365ebc20ab9f16988ccfce8fe6eeb
Cees-Jan Swart <cees-jan.swart@kpn.com>
2026-04-08 14:06:16 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
   public static void main (String [] args) {
     System.out.println("Hello Cees-Jan en Jeroen by docker automatic build");
-    System.exit(0);
+    password = 'geheim'
+    System.exit(1);
   }
 }
